18629d6b6e78df20e58e75bd5b43e3be335f3897
JokerP515 <106451105+JokerP515@users.noreply.github.com>
2023-11-16 22:45:13 -0500

    Add files via upload

---
+++ TABLAS CAMIONES.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS transporte;
+USE transporte;
+
+CREATE TABLE IF NOT EXISTS camiones (
+    matricula VARCHAR(20) PRIMARY KEY,
+    capacidad DOUBLE,
+    consumo DOUBLE,
+    cargaActual DOUBLE
+);
+
+CREATE TABLE IF NOT EXISTS cargas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    descripcion VARCHAR(100),
+    peso DOUBLE,
+    camion_matricula VARCHAR(20),
+    FOREIGN KEY (camion_matricula) REFERENCES camiones(matricula)
+);
+
+ALTER TABLE cargas AUTO_INCREMENT = 1;
